d8ff90a6458e5525293627bb1abe933f492b750e
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2020-11-18 14:24:53 +1100

    Add tests and comments

--- tests/mock.py
+++ tests/mock.py
@@ -2,30 +2,51 @@ import pandas as pd
 import numpy as np
 from pprint import pprint
 
-def make_dataset(nrows: float=100, specs: dict=None, missing: bool=False) -> pd.DataFrame:
+def mock_dataset(specs: dict=None,):
     """
     Create mock pandas dataframe
 
     Args:
-        nrows (float, optional): number of rows. Defaults to 100.
         specs (dict, optional): Specifications of the data frame. Defaults to None.
-        missing (bool, optional): Add missing values. Defaults to False.
 
     Returns:
         pd.DataFrame: mock pandas dataframe
+
+    Example:
+        >>> specs = {"float": [100, 1, 0.05] \
+                    ,"integer": [100, 1, 0.025] \
+                    ,"categorical": [100, 1, 0.1] \
+                    ,"boolean": [100, 1, 0] \
+                    ,"string": [100, 1, 0] \
+                    }
+        >>> df = mock_dataset(specs)
+        >>> df.shape[0] == 100
+        True
+        >>> df.shape[1] == 5
+        True
+        >>> df.isnull().sum()["float_0"] / df.shape[0] == 0.05
+        True
+        >>> df.isnull().sum()["integer_0"] / df.shape[0] == 0.02
+        True
+        >>> df.isnull().sum()["categorical_0"] / df.shape[0] == 0.1
+        True
+        >>> df.isnull().sum()["boolean_0"] / df.shape[0] == 0
+        True
+        >>> df.isnull().sum()["string_0"] / df.shape[0] == 0
+        True
     """
 
-    if nrows <= 4:
-        nrows = 5
-
+    # 1. Build specs, in case needed
     if not specs:
-        specs = {"float": [nrows, np.random.randint(1, 4), np.random.rand()]
-                ,"integer": [nrows, np.random.randint(1, 4), np.random.rand()]
-                ,"categorical": [nrows, np.random.randint(1, 4), 0.75]
-                ,"boolean": [nrows, np.random.randint(1, 4), np.random.rand()]
-                ,"string": [nrows, np.random.randint(1, 4), np.random.rand()]
+        # Format of specs dict: {data_type: [nrows, ncols, nnulls]}
+        specs = {"float": [100, np.random.randint(1, 4), np.random.rand()]
+                ,"integer": [100, np.random.randint(1, 4), np.random.rand()]
+                ,"categorical": [100, np.random.randint(1, 4), 0.75]
+                ,"boolean": [100, np.random.randint(1, 4), np.random.rand()]
+                ,"string": [100, np.random.randint(1, 4), np.random.rand()]
                 }
 
+    # 2. Build values of the data frame
     values = {}
     for col_type, col_spec in specs.items():
         if col_type == "float":
@@ -50,16 +71,11 @@ def make_dataset(nrows: float=100, specs: dict=None, missing: bool=False) -> pd.
 
     df = pd.DataFrame.from_dict(values)
 
-    if missing:
-        for col_type, col_spec in specs.items():
-            selected_cols = [col for col in df.columns.values if col_type in col]
-            for col in set(selected_cols):
-                mask = df[col].sample(frac=col_spec[2]).index
-                df.loc[mask, col] = np.nan
+    # 3. Add nulls according to the proportion specified
+    for col_type, col_spec in specs.items():
+        for col in [col for col in df.columns.values if col_type in col]:
+            mask = df[col].sample(frac=col_spec[2]).index
+            df.loc[mask, col] = np.nan
 
     return df
 
-
-if __name__ == "__main__":
-    df = make_dataset(missing=True)
-    pprint(df)
